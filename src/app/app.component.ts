fcd0537d8782afcc1aba7ce4b70ed875cc037c3f
Stefan <stefansandorovssb@gmail.com>
2023-06-27 17:03:36 +0200

    pasar datos entre componentes hijos y padres manualmente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '02-redux-app';
+
+  contador: number;
+
+  constructor() {
+    this.contador = 10;
+  }
+
+  incrementar() {
+    this.contador += 1;
+  }
+
+  reducir() {
+    this.contador -= 1;
+  }
 }
