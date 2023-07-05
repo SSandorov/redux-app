2152efff40e5c2c65bbf61bcd58664030d3b69d7
Stefan <stefansandorovssb@gmail.com>
2023-07-05 16:41:47 +0200

    método para crear el reducer con ngRx

--- src/app/contador/contador.reducer.ts
+++ src/app/contador/contador.reducer.ts
@@ -1,17 +1,29 @@
-import { Action } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
 import { incrementar, reducir } from "./contador.actions";
 
 // * Implementamos a mano el reducer
-export function contadorReducer(state: number = 10, action: Action) {
-  switch(action.type) {
+// export function contadorReducer(state: number = 10, action: Action) {
+//   switch(action.type) {
 
-    case incrementar.type:
-      return state + 1;
+//     case incrementar.type:
+//       return state + 1;
 
-    case reducir.type:
-      return state - 1;
+//     case reducir.type:
+//       return state - 1;
 
-    default:
-      return state;
-  }
-}
+//     default:
+//       return state;
+//   }
+// }
+
+/*
+  * El uso del createReducer() es más eficiente al no emplear switches para manejar los
+  * distintos casos
+*/
+export const initialState = 10;
+
+export const contadorReducer = createReducer(
+  initialState,
+  on(incrementar, (state) => state + 1),
+  on(reducir, (state) => state - 1),
+);
