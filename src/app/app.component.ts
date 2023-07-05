951ce739d5cd2eff3f30833a5baee583ef63b65d
Stefan <stefansandorovssb@gmail.com>
2023-07-05 16:25:11 +0200

    esuchar cambios de un elemento del store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,13 +19,21 @@ export class AppComponent {
   contador!: number;
 
   constructor(
-    // * injectamos el servicio store y le añadimos el tipo
+    //* injectamos el servicio store y le añadimos el tipo
     private store: Store<AppState>
     ) {
-      // * podemos elegir si suscribirnos en el constructor o en el ngOnInit
-      this.store.subscribe(state =>{
-        this.contador = state.contador;
-      })
+      //* podemos elegir si suscribirnos en el constructor o en el ngOnInit
+      //* Esta es la manera global de acceder al state, donde tenemos que elegir el elemento
+      //* con el que queremos trabajar
+      // this.store.subscribe(state =>{
+      //   this.contador = state.contador;
+      // })
+      /*
+      * Si vemos las diferentes opciones dentro del store, podemos comprobar que tiene
+      * varios métodos, entre ellos el select
+      * El método select nos permite seleccionar un método en específico del store
+      */
+      this.store.select('contador').subscribe(contador =>this.contador = contador);
     }
 
   incrementar() {
